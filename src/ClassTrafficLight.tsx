a25670d27a4ad438179c69871c8e8b82d8c6047e
Michael Tavarez-Feliciano <mtavarezfeliciano@gmail.com>
2024-04-04 15:42:37 -0400

    class and func comp done i think

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,26 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+
+  state = {
+    status: 'red',
+  };
+
+  checkStatus = (color: string) => (this.state.status === color ? color: 'black');
+  nextColor = () => {
+    this.setState({status: this.state.status === 'red' ? 'yellow' : this.state.status === 'yellow' ? 'green' : 'red'})
+  }
+
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${this.checkStatus('red')}`}></div>
+          <div className={`circle ${this.checkStatus('yellow')}`}></div>
+          <div className={`circle ${this.checkStatus('green')}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.nextColor}>Next State</button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,14 +1,22 @@
-export const FunctionalTrafficLight = () => {
+import React, { useState } from "react";
+
+export const FunctionalTrafficLight: React.FC = () => {
+
+  const [status, setStatus] = useState('red');
+  const checkStatus = (color: string) => (status === color ? color: 'black');
+  const nextColor = () => {
+    setStatus(status === 'red' ? 'yellow' : status === 'yellow' ? 'green' : 'red');
+  }
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${checkStatus("red")}`}></div>
+        <div className={`circle ${checkStatus("yellow")}`}></div>
+        <div className={`circle ${checkStatus("green")}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={nextColor}>Next State</button>
     </div>
   );
 };
